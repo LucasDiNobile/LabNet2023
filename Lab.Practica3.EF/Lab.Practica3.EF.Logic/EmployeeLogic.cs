be4698983352dcedd2026bab6aac9ad5b1d712ab
Lucas Di Nobile <lucasdinobile9@gmail.com>
2023-08-29 22:23:06 -0300

    Avances
    Se comienza la lógica del menu y se realizan métodos
    GetAll, Insert y Update.

---
+++ Lab.Practica3.EF/Lab.Practica3.EF.Logic/EmployeeLogic.cs
@@ -0,0 +1,58 @@
+﻿using Lab.Practica3.EF.Data;
+using Lab.Practica3.EF.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab.Practica3.EF.Logic
+{
+    public class EmployeeLogic : BaseLogic, ILogic<Employee>
+    {
+
+        public EmployeeLogic() : base() { }
+
+
+        public List<Employee> GetAll()
+        {
+            return context.Employees.ToList();
+        }
+
+        public void Delete(Employee entity)
+        {
+            var employeeToDelete = context.Employees.Find(entity.EmployeeID);
+
+            context.Employees.Remove(employeeToDelete);
+
+            context.SaveChanges();
+        }
+        public void Insert(Employee entity)
+        {
+            context.Employees.Add(entity);
+
+            context.SaveChanges();
+        }
+        public void Update(Employee entity)
+        {
+            var employeeUpdate = context.Employees.Find(entity.EmployeeID);
+
+            employeeUpdate.LastName = entity.LastName;
+            employeeUpdate.FirstName = entity.FirstName;
+            employeeUpdate.Title = entity.Title; 
+            employeeUpdate.TitleOfCourtesy = entity.TitleOfCourtesy;
+            employeeUpdate.BirthDate = entity.BirthDate;
+            employeeUpdate.HireDate = entity.HireDate;
+            employeeUpdate.Address = entity.Address;
+            employeeUpdate.City = entity.City;
+            employeeUpdate.Region = entity.Region;
+            employeeUpdate.PostalCode = entity.PostalCode;
+            employeeUpdate.Country = entity.Country;    
+            employeeUpdate.HomePhone = entity.HomePhone;
+            employeeUpdate.Extension = entity.Extension;
+            employeeUpdate.Notes = entity.Notes;
+
+            context.SaveChanges();
+        }
+    }
+}
--- Lab.Practica3.EF/Lab.Practica3.EF.UI/Program.cs
+++ Lab.Practica3.EF/Lab.Practica3.EF.UI/Program.cs
@@ -1,8 +1,12 @@
-﻿using System;
+﻿using Lab.Practica3.EF.Logic;
+using Lab.Practica3.EF.Entities;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Security.Policy;
+using System.Runtime.CompilerServices;
 
 namespace Lab.Practica3.EF.UI
 {
@@ -10,6 +14,133 @@ namespace Lab.Practica3.EF.UI
     {
         static void Main(string[] args)
         {
+            bool continuar = true;
+            int tablaDeseada = 0, accionDeseadaCliente = 0, accionDeseadaEmpleado = 0;
+            EmployeeLogic employeeLogic = new EmployeeLogic();
+
+            while (continuar) 
+            {
+                try
+                {
+                    Console.WriteLine("Que tabla desea utilizar?\n 1.Clientes\n 2.Empleados\n 0.Salir de la aplicacion");
+                    tablaDeseada = int.Parse(Console.ReadLine());
+                }
+                catch (FormatException ex)
+                {
+                    Console.WriteLine(ex.Message);
+                }
+
+                switch (tablaDeseada)
+                {   case 1:
+                        Console.WriteLine("Ustedes elijio la tabla de clientes, desea:\n" +
+                            "1.Ver tabla de clientes\n" +
+                            "2.Insertar un nuevo cliente\n" +
+                            "3.Modificar un cliente\n" +
+                            "4.Eliminar un cliente\n" +
+                            "0.Salir de la aplicacion");
+                        accionDeseadaCliente = int.Parse(Console.ReadLine());
+                        break;
+                    case 2:
+                        Console.WriteLine("Ustedes elijio la tabla de empleados, desea:\n" +
+                            "1.Ver tabla de empleados\n" +
+                            "2.Insertar un nuevo empleado\n" +
+                            "3.Modificar un empleado\n" +
+                            "4.Eliminar un empleado\n" +
+                            "0.Salir de la aplicacion");
+                        accionDeseadaEmpleado = int.Parse(Console.ReadLine());
+                        break;
+                    case 0:
+                        continuar = false;
+                        break;
+                    default:
+                        Console.WriteLine("el numero no es valido, vuelva a intentarlo");
+                        break;
+                }
+
+                switch (accionDeseadaCliente)
+                {
+                    case 1:
+                        break;
+                    case 2:
+                        break; 
+                    case 3:
+                        break;
+                    case 4:
+                        break;
+                    case 0:
+                        break;
+                }
+
+                switch (accionDeseadaEmpleado)
+                {
+                    case 1:
+                        foreach (Employee employee in employeeLogic.GetAll())
+                            {
+                              Console.WriteLine($"{employee.FirstName} {employee.LastName} from {employee.Country }, was hired on {employee.HireDate}\n");
+                            }
+                        break;
+                    case 2:  //TODO                                                                                              
+                        employeeLogic.Insert(new Employee
+                        {
+                          FirstName = Console.ReadLine(),
+                          LastName = "Di Nobile"
+                        });
+                        break;
+                    case 3:
+                        break;
+                    case 4:
+                        break;
+                    case 0:
+                        break;
+                }
+
+
+            }
+
+            
+            foreach (var item in employeeLogic.GetAll())
+            {
+                Console.WriteLine($"{item.EmployeeID} {item.FirstName} {item.LastName} {item.HireDate}");
+            }
+
+            Console.ReadKey();
         }
     }
 }
+
+
+//Muestra tabla de empleados
+//EmployeeLogic employeeLogic = new EmployeeLogic();
+
+//foreach (Employee employee in employeeLogic.GetAll())
+//{
+//  Console.WriteLine($"{employee.FirstName} {employee.LastName} from {employee.Country }, was hired on {employee.HireDate}\n");
+//}
+
+
+//Prueba Insert
+
+//employeeLogic.Insert(new Employee
+//{
+//  FirstName = "Lucas",
+//  LastName = "Di Nobile"
+//});
+
+//foreach (var item in employeeLogic.GetAll())
+//{
+//  Console.WriteLine($"{item.EmployeeID} {item.FirstName} {item.LastName} {item.HireDate}");
+//}
+
+
+// Prueba Delete
+//employeeLogic.Delete(new Employee { EmployeeID = 9 });
+
+
+// Prueba Update
+
+//employeeLogic.Update(new Employee
+//{
+//   LastName = "Cruz",
+//    FirstName = "Juan",         
+//     EmployeeID = 2,
+//});
