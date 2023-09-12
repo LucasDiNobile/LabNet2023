cc81bf510f431d178d7e5215c01938cf21fdad2f
Lucas Di Nobile <lucasdinobile9@gmail.com>
2023-09-12 15:37:51 -0300

    Ejercicio 1 Api Publica
    Se agrega manejo de Api Publica

---
+++ Lab.Practica3.EF/Lab.Practica6.MVC/Controllers/ApiPublicaController.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Lab.Practica6.MVC.Controllers
+{
+    public class ApiPublicaController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult VolverInicio()
+        {
+            return RedirectToAction("Index", "Home");
+        }
+
+        public ActionResult Recargar()
+        {
+            return RedirectToAction("Index");
+        }
+    }
+}
